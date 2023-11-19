5cc3e4c91f707a90031b20306419013d8cf2ec15
KiminSon <kimin0331@naver.com>
2023-11-20 00:44:29 +0900

    feat: 회원가입 기능 구현
    백엔드 회원가입 기능 구현

--- DongDongYee/src/main/java/dd_user/DD_UserDAO.java
+++ DongDongYee/src/main/java/dd_user/DD_UserDAO.java
@@ -23,7 +23,7 @@ public class DD_UserDAO {
 	}
 	// 로그인 기능 
 	public int login(String UserID, String UserPassword) {
-		String SQL = "SELECT UserPassword FROM USER WHERE UserID = ?";
+		String SQL = "SELECT UserPassword FROM DD_USER WHERE UserID = ?";
 		try {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, UserID); //sql Injection 공격 방어 수단 : 1번째 물음표에 userID 입력
@@ -40,5 +40,54 @@ public class DD_UserDAO {
 			
 		}
 		return -2; //DB 오류 
+	}
+	
+	public int join(DD_User user) {
+	    // 이메일 중복 체크
+	    String checkEmailSQL = "SELECT COUNT(*) FROM DD_USER WHERE UserEmail = ?";
+	    try {
+	        PreparedStatement checkEmailStmt = conn.prepareStatement(checkEmailSQL);
+	        checkEmailStmt.setString(1, user.getUserEmail());
+	        ResultSet rsEmail = checkEmailStmt.executeQuery();
+	        if (rsEmail.next()) {
+	            if (rsEmail.getInt(1) > 0) {
+	                return -2; // 이메일이 중복되면 -2 반환
+	            }
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return -1; // DB 오류
+	    }
+		
+		// 중복 닉네임 체크
+	    String checkSQL = "SELECT COUNT(*) FROM DD_USER WHERE UserNickname = ?";
+	    try {
+	        PreparedStatement checkStmt = conn.prepareStatement(checkSQL);
+	        checkStmt.setString(1, user.getUserNickname());
+	        ResultSet rs = checkStmt.executeQuery();
+	        if (rs.next()) {
+	            if (rs.getInt(1) > 0) {
+	                return -3; // 닉네임이 중복되면 -3 반환
+	            }
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return -1; // DB 오류
+	    }
+	    
+	    //등록
+		String SQL = "INSERT INTO DD_USER VALUES(?, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserEmail());
+			pstmt.setString(4, user.getUserNickname());
+			return pstmt.executeUpdate(); // 0이상 값이 return된 경우 성공 
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		return -1; //DB 오류 
 	}
 }
