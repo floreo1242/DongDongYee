ea171ec7d88254242212819803bb59ebb32861ca
Jinsoo Yoon <floreo1242@gmail.com>
2023-12-16 22:47:33 +0900

    Feat: 프로필 및 회원탈퇴 기능 추가

---
+++ DongDongYee/src/main/java/domain/user/usercontroller/UserServlet.java
@@ -0,0 +1,56 @@
+package domain.user.usercontroller;
+
+import domain.user.User;
+import domain.user.UserService;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/Profile")
+public class UserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserService userService = new UserService();
+
+    public UserServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("UTF-8");
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userID") == null) {
+            response.sendRedirect("Login.jsp");
+            return;
+        }
+        User user = userService.read(session.getAttribute("userID").toString());
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("Profile.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("UTF-8");
+        System.out.println("POST");
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userID") == null) {
+            response.sendRedirect("Login.jsp");
+            return;
+        }
+        String userPassword = userService.findUserPasswordByUserId(session.getAttribute("userID").toString());
+        String password = request.getParameter("password");
+        if (password.equals(userPassword)) {
+            System.out.println("IF");
+            userService.delete(session.getAttribute("userID").toString());
+            response.sendRedirect("Login.jsp");
+        } else {
+            System.out.println("ELSE");
+            response.sendRedirect("Profile");
+        }
+    }
+}
